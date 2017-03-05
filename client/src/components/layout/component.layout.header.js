4ef20eadd733b3f668a2523756422c5bd47fd89c
Don <helmwebdev@gmail.com>
2017-03-05 01:47:03 -0600

    start making site responsive

---
+++ client/src/components/layout/component.layout.header.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  NavLink
+} from 'react-router-dom';
+
+import HamMenu from 'material-ui/svg-icons/navigation/menu';
+import classNames from 'classnames';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.handleNavClick = this.handleNavClick.bind(this);
+
+    this.state = {
+      mobileNavShown: false
+    };
+  }
+
+  handleNavClick () {
+    this.setState({'mobileNavShown': !this.state.mobileNavShown});
+  }
+
+  render() {
+    const dynamicNavClasses = classNames({
+      'is-Shown': this.state.mobileNavShown
+    });
+
+    const navClasses = 'col-xs-12 col-md-8 c-Header__nav ' + dynamicNavClasses;
+
+    return (
+      <header className="c-Site__header c-Header">
+        <div className="container-fluid c-Header__top">
+          <div className="c-Header__top-row">
+            <div className="row c-Header__logo-wrap">
+              <div className="col-xs-12 col-md-2 c-Header__logo-desk-wrap">
+                <div className="row between-xs c-Header__logo-desk-inner-wrap">
+                  <h1 className="col-xs-9 col-md-3 c-Header__logo">
+                    <NavLink to="/">Beyond the Spectrum</NavLink>
+                  </h1>
+                  <div className="col-xs-offset-1 col-xs-2 col-md-0 c-Header__ham">
+                    <HamMenu color={'#fff'} onClick={this.handleNavClick} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <nav className={navClasses}>
+              <ul onClick={this.handleNavClick}
+                className="o-List-bare c-Header__list">
+                <li className="c-Header__item">
+                  <NavLink
+                    to="/resources/"
+                    className="c-Header__link"
+                    activeClassName="is-Active"
+                  >
+                    Resources
+                  </NavLink>
+                </li>
+                <li className="c-Header__item">
+                  <NavLink
+                    to="/resources/submit-resource/"
+                    className="c-Header__link"
+                    activeClassName="is-Active"
+                  >
+                    Submit a Resource
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            <nav className="col-md-2 c-Header__signin">
+              <ul className="o-List-bare c-Header__list">
+                <li className="c-Header__item">
+                  <NavLink
+                    to="/resources/"
+                    className="c-Header__link"
+                    activeClassName="is-Active"
+                  >
+                    Sign In
+                  </NavLink>
+                </li>
+                <li className="c-Header__item">
+                  <NavLink
+                    to="/resources/submit-resource/"
+                    className="c-Header__link"
+                    activeClassName="is-Active"
+                  >
+                    Join
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>{/* /.c-Header__top */}
+        <div className="c-Header__bg-color">
+          <div className="c-Header__bg-img"></div>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Header;
+
--- client/src/components/layout/component.layout.master.js
+++ client/src/components/layout/component.layout.master.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-  Route,
-  NavLink,
-  Link
+  Route
 } from 'react-router-dom';
+
+import Header from './component.layout.header';
 
 import HomePage from '../pages/component.page.home';
 import ResourcesPage from '../pages/component.page.resources';
@@ -11,64 +11,7 @@ import ResourcesPage from '../pages/component.page.resources';
 const MasterLayout = ({ match }) => {
   return (
     <div>
-      <header className="c-Site__header c-Header">
-        <div className="container-fluid c-Header__top">
-          <div className="row c-Header__top-row">
-            <div className="col-md-2 c-Header__logo-wrap">
-              <h1 className="c-Header__logo">
-                <Link to="/">Beyond the Spectrum</Link>
-              </h1>
-            </div>
-            <nav className="col-md-8 c-Header__nav">
-              <ul className="o-List-bare c-Header__list">
-                <li className="c-Header__item">
-                  <NavLink
-                    to="/resources/"
-                    className="c-Header__link"
-                    activeClassName="is-Active"
-                  >
-                    Resources
-                  </NavLink>
-                </li>
-                <li className="c-Header__item">
-                  <NavLink
-                    to="/resources/submit-resource/"
-                    className="c-Header__link"
-                    activeClassName="is-Active"
-                  >
-                    Submit a Resource
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-            <nav className="col-md-2 c-Header__signin">
-              <ul className="o-List-bare c-Header__list">
-                <li className="c-Header__item">
-                  <NavLink
-                    to="/resources/"
-                    className="c-Header__link"
-                    activeClassName="is-Active"
-                  >
-                    Sign In
-                  </NavLink>
-                </li>
-                <li className="c-Header__item">
-                  <NavLink
-                    to="/resources/submit-resource/"
-                    className="c-Header__link"
-                    activeClassName="is-Active"
-                  >
-                    Join
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>{/* /.c-Header__top */}
-        <div className="c-Header__bg-color">
-          <div className="c-Header__bg-img"></div>
-        </div>
-      </header>
+      <Header />
 
       <div className="container-fluid c-Site__content">
         <Route path={`${match.url}`} exact component={HomePage} />
